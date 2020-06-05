5917fb181b831cf5be19d2fb4b17f1f41b14fd11
Az3r <tuan99kghd@gmail.com>
2020-06-05 16:54:00 +0700

    + add a view for example

---
+++ dbo/Views/StudentInfo.sql
@@ -0,0 +1,3 @@
+﻿CREATE VIEW [dbo].[StudentInfo]
+    AS SELECT Student.MaHocSinh, DaRaTruong, Ho, Ten, TenDem 
+    FROM [Student] join [StudentDetail] on Student.MaHocSinh = StudentDetail.MaSoSinhVien
