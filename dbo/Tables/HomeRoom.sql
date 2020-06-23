ed9180c19ce62f57f8d2ec5f00d3667e21171167
Azer <azerprojects@gmail.com>
2020-06-23 16:41:01 +0700

    - change properties' names in HomeRoo, table

--- dbo/Tables/HomeRoom.sql
+++ dbo/Tables/HomeRoom.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [HomeRoom] (
-    [MaLop] char(4)  NOT NULL PRIMARY KEY,
-    [MaGiaoVien] char(9)  NOT NULL ,
-    [NienKhoa] int  NOT NULL ,
-    CONSTRAINT [FK_HomeRoom_Teacher] FOREIGN KEY([MaGiaoVien]) REFERENCES [Teacher] ([TeacherId]) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT [FK_HomeRoom_Class] FOREIGN KEY([MaLop]) REFERENCES [Class] ([ClassId]) ON DELETE CASCADE ON UPDATE CASCADE,
+    [ClassId] char(4)  NOT NULL PRIMARY KEY,
+    [TeacherId] char(9)  NOT NULL ,
+    [AcademicYear] int  NOT NULL ,
+    CONSTRAINT [FK_HomeRoom_Teacher] FOREIGN KEY([TeacherId]) REFERENCES [Teacher] ([TeacherId]) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT [FK_HomeRoom_Class] FOREIGN KEY([ClassId]) REFERENCES [Class] ([ClassId]) ON DELETE CASCADE ON UPDATE CASCADE,
 )
