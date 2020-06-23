aa382fbea71a2cc3bca34ea39cce94c8b93e8646
Azer <azerprojects@gmail.com>
2020-06-23 16:53:10 +0700

    - change properties' names in Subject table

--- dbo/Tables/Subject.sql
+++ dbo/Tables/Subject.sql
@@ -1,10 +1,10 @@
 ﻿CREATE TABLE [Subject] (
     [SubjectId] int IDENTITY(1,1) NOT NULL ,
-    [Ten] nvarchar(50)  NOT NULL ,
+    [SubjectName] nvarchar(50)  NOT NULL ,
     CONSTRAINT [PK_Subject] PRIMARY KEY CLUSTERED (
         [SubjectId] ASC
     ),
-    CONSTRAINT [UK_Subject_Ten] UNIQUE (
-        [Ten]
+    CONSTRAINT [UK_SubjectName] UNIQUE (
+        [SubjectName]
     )
 )
